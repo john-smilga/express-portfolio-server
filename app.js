fe1807c04a23685e3ee33dfe84190322109466a4
John <johnsmilga@gmail.com>
2019-11-26 06:00:29 -0800

    cors issues fixed

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const products = require("./products");
+const cors = require("cors");
+
+app.use(cors());
 
 app.get("/", function(req, res) {
   res.send("Hello World!");
