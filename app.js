53d0ab79d5876ee392fc831efca643facb30a564
John <johnsmilga@gmail.com>
2019-11-26 05:32:06 -0800

    root route added

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const products = require("./products");
 
+app.get("/", function(req, res) {
+  res.send("Hello World!");
+});
 app.get("/projects/react-tech-store-v2", function(req, res) {
   res.json(products);
 });
