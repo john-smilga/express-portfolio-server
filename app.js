519f613b0d99171d88fdf8087224d0aee7753273
John <johnsmilga@gmail.com>
2020-06-25 13:38:52 -0700

    javascript tutorial project added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const products = require("./projects/react/vintage-tech/products");
+const people = require("./projects/javascript/tutorial/people");
 const cors = require("cors");
 
 app.use(cors());
@@ -10,6 +11,9 @@ app.get("/", function (req, res) {
 });
 app.get("/projects/react-tech-store-v2/products", function (req, res) {
   res.json(products);
+});
+app.get("/projects/tutorial/people", function (req, res) {
+  res.json(people);
 });
 
 app.listen(process.env.PORT || 3000);
---
+++ projects/javascript/tutorial/people.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    name: "john",
+    id: 1,
+  },
+  {
+    name: "peter",
+    id: 2,
+  },
+  {
+    name: "anna",
+    id: 3,
+  },
+  {
+    name: "susan",
+    id: 4,
+  },
+];
